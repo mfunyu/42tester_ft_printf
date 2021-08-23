a2fa6359f63ea90d63f61bfe13c0953c316498a0
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-23 23:55:23 +0900

    [fix] declarations

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...);
 
 #ifdef REAL
 # define P(...) printf(__VA_ARGS__)
 #else
 # define P(...) ft_printf(__VA_ARGS__)
+#endif
 
-int		main()
+int	main()
 {
 	int		ret;
 
 	ret = 0;
-	r += P("---Start Test---\n");
+	ret += P("---Start Test---\n");
 	printf("written total: %d", ret);
 }
