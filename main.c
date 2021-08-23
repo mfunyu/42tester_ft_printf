94737bfbdeb678815a9b2de99ad36c7d6b9a7a6e
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-24 00:32:17 +0900

    [update] add tests, sep funcs

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <limits.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 
 #ifdef REAL
 # define P(...) printf(__VA_ARGS__)
@@ -8,11 +9,55 @@ int	ft_printf(const char *format, ...);
 # define P(...) ft_printf(__VA_ARGS__)
 #endif
 
-int	main()
+int	mandatory(void)
+{
+	int		ret;
+	char	*nullstr = NULL;
+	int		ft = 42;
+
+	ret = 0;
+	ret += P("---Start Test---\n");
+	ret += P("---%%c---\n");
+	ret += P("---%%s---\n");
+	ret += P("%s\n", "hello world!");
+	ret += P("%s\n", nullstr);
+	// ret += P("%s\n", NULL);
+	// ret += P("hello %s %s \n", "world", NULL);
+	ret += P("---%%d---\n");
+	ret += P("%d\n", ft);
+	ret += P("---%%i---\n");
+	ret += P("---%%u---\n");
+	ret += P("---%%x---\n");
+	ret += P("---%%X---\n");
+	return (ret);
+}
+
+int	bonus(void)
+{
+	int		ret;
+	char	*nullstr = NULL;
+	int		ft = 42;
+
+	ret = 0;
+	ret += P("%-s\n", nullstr);
+	ret += P("%2s\n", nullstr);
+	ret += P("%10s\n", nullstr);
+	ret += P("%2.8s\n", nullstr);
+	ret += P("%2.3s\n", nullstr);
+	ret += P("%3.0d %3.0d %3.0d %3.0d\n", 0, 42, INT_MAX, INT_MIN);
+	ret += P("%.1d\n", ft);
+	return (ret);
+}
+
+int	main(void)
 {
 	int		ret;
 
 	ret = 0;
-	ret += P("---Start Test---\n");
-	printf("written total: %d", ret);
+	#ifndef BONUS
+		ret += mandatory();
+	#else
+		ret += bonus();
+	#endif
+	printf("written total: %d\n", ret);
 }
